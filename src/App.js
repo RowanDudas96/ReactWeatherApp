7896193ac5e51e50431ff94de2f83745d1fb7813
Rowan Dudas <rowan.dudas@gmail.com>
2020-08-21 15:41:24 +0100

    finished app commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,106 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+// api keys and base
+const api = {
+  key: "e9864734d221d5fd3ec75782d4b11c1f",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+  // api query functions
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(weather);
+        });
+    }
+  };
+  // function to display months and days
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
+    //variables for collecting the date information
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    // date display
+    return `${day} ${date} ${month} ${year}`;
+  };
+
+  return (
+    // if statement for different weather conditions. e.g. if weather is over 16°c, then change background of app.
+    <div
+      className={
+        typeof weather.main !== "undefined"
+          ? weather.main.temp > 16
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
+      <main>
+        {/* search box and functions assigned */}
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+        {/* check for seeing if the search box is not equal to null. if not, then an api request will be sent, and the data will be returned to the app. */}
+        {typeof weather.main !== "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
+    </div>
+  );
 }
 
 export default App;
